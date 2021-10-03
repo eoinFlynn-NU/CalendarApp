42199fbe486e42ccbe51025593a9a22e633444ac
nickselvitelli <nickselvitelli>
2021-10-03 13:07:37 -0400

    added files for server things. getting deprecation warnings

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+const db = require("./models");
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
@@ -26,3 +33,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+

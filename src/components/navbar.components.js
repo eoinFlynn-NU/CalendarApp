2a438d0f7fe758eb7c50d5217bdc5d5c737b4122
Sam <phillippo.s@northeastern.edu>
2021-10-23 15:35:32 -0400

    Created a nav-bar, home page, and a tictactoe board

--- src/components/navbar.components.js
+++ src/components/navbar.components.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default class LoginNavigation extends Component {
+export default class Navigation extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
     <div className="container">
-      <Link className="navbar-brand" to={"/sign-in"}>Selvel7i</Link>
+      <Link className="navbar-brand" to={"/home"}>Selvel7i</Link>
       <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link className="nav-link" to={"/sign-in"}>Login</Link>
+            <Link className="nav-link" to={"/home"}>Home</Link>
           </li>
-          <div />
           <li className="nav-item">
-            <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+            <Link className="nav-link" to={"/tictactoe"}>TicTacToe</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={"/"}>Log out</Link>
           </li>
         </ul>
       </div>

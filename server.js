b8f85d33e8771bc4fe5ee98145c420572f8b0876
nickselvitelli <nickselvitelli>
2021-10-24 12:36:39 -0400

    backend crap

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
+// parse requests of content-type: application/json
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
+// parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-const db = require("./models");
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
-
 
 // simple route
 app.get("/", (req, res) => {
@@ -29,8 +15,6 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
 });
-
